0ce5b137f14ffac5dc190665dc26ff225255e9c7
pai <paiuolo@gmail.com>
2020-12-30 12:54:10 +0100

    search

--- todo/views/search.py
+++ todo/views/search.py
@@ -5,19 +5,33 @@ from django.shortcuts import render
 
 from todo.models import Task
 from todo.utils import staff_check
+from todo.forms import SearchForm
 
 
 @login_required
-@user_passes_test(staff_check)
+# @user_passes_test(staff_check)  # pai
 def search(request) -> HttpResponse:
     """Search for tasks user has permission to see.
     """
 
     query_string = ""
 
-    if request.GET:
+    if request.POST:
+        form = SearchForm(request.POST)
 
-        found_tasks = None
+        if form.is_valid():
+            query_string = form.cleaned_data.get('q', '')
+
+    else:
+        if ("q" in request.GET) and request.GET["q"].strip():
+            query_string = request.GET["q"]
+
+        initial = {'q': query_string}
+        form = SearchForm(initial=initial)
+
+    context = {"form": form}
+
+    if query_string != '':
         if ("q" in request.GET) and request.GET["q"].strip():
             query_string = request.GET["q"]
 
@@ -29,15 +43,20 @@ def search(request) -> HttpResponse:
             # We still need found_tasks in a queryset so it can be "excluded" below.
             found_tasks = Task.objects.all()
 
-        if "inc_complete" in request.GET:
-            found_tasks = found_tasks.exclude(completed=True)
-
     else:
         found_tasks = None
 
-    # Only include tasks that are in groups of which this user is a member:
-    if not request.user.is_superuser:
-        found_tasks = found_tasks.filter(task_list__group__in=request.user.groups.all())
 
-    context = {"query_string": query_string, "found_tasks": found_tasks}
+    # Only include tasks that are in groups of which this user is a member:
+    # if not request.user.is_superuser:
+    if found_tasks:
+        if "inc_complete" in request.GET:
+            found_tasks = found_tasks.exclude(completed=True)
+
+        if not staff_check(request.user):  # pai
+            found_tasks = found_tasks.filter(task_list__group__in=request.user.groups.all())
+
+    context["query_string"] = query_string
+    context["found_tasks"] = found_tasks
+
     return render(request, "todo/search_results.html", context)
