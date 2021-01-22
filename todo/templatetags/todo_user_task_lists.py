0956e8d373f4869b127e6f3007510b998f640f0f
pai <paiuolo@gmail.com>
2021-01-22 13:59:41 +0100

    fix templatetag import

--- todo/templatetags/todo_user_task_lists.py
+++ todo/templatetags/todo_user_task_lists.py
@@ -1,7 +1,7 @@
 from django import template
 
 from ..models import TaskList
-from ..utils import staff_check
+from ..utils import staff_check, get_user_groups
 
 register = template.Library()
 
@@ -9,6 +9,6 @@ register = template.Library()
 @register.simple_tag
 def todo_user_task_lists(user):
     if staff_check(user):
-        return TaskList.objects.filter(is_active=True)
+        return TaskList.objects.filter(is_active=True).order_by('group__name')
     else:
-        return TaskList.objects.filter(is_active=True).filter(group__in=get_user_groups(request.user))
+        return TaskList.objects.filter(is_active=True).filter(group__in=get_user_groups(user)).order_by('group__name')
