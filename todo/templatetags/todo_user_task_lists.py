c2b9594ee332ed276d646533c27e9d5dc1d4fe6c
pai <paiuolo@gmail.com>
2021-02-02 15:10:38 +0100

    templatetag ordering

--- todo/templatetags/todo_user_task_lists.py
+++ todo/templatetags/todo_user_task_lists.py
@@ -8,7 +8,11 @@ register = template.Library()
 
 @register.simple_tag
 def todo_user_task_lists(user):
+    """
+    Order vy group__name for template regroup
+    """
     if staff_check(user):
-        return TaskList.objects.filter(is_active=True).order_by('group__name')
+        return TaskList.objects.filter(is_active=True).order_by('group__name', 'created_at')
     else:
-        return TaskList.objects.filter(is_active=True).filter(group__in=get_user_groups(user)).order_by('group__name')
+        return TaskList.objects.filter(is_active=True).filter(group__in=get_user_groups(user)).order_by('group__name',
+                                                                                                        'created_at')
